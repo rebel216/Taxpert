685824393e5a59b7b5005ad19836ab9d59171130
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-11-21 17:17:15 +0530

    Made Page live

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
 gem 'devise'
+gem 'sendotp', '~> 0.1.0'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
+  validates :phone, uniqueness: true
+
+def email_required?
+  false
+end
+
+def email_changed?
+  false
+end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
