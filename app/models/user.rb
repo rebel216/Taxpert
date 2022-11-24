b34863583d507d77122b30c3a67e275e2f355089
Neeraj Bhardwaj <neerajbhardwajpkt@gmail.com>
2022-11-24 18:04:45 +0530

    Devise error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,13 +1,15 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   validates :phone, uniqueness: true
 
-def email_required?
-  false
-end
+  def email_required?
+    false
+  end
 
-def email_changed?
-  false
-end
+  def email_changed?
+    false
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
--- spec/requests/home_spec.rb
+++ spec/requests/home_spec.rb
@@ -1,8 +1,10 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe "Homes", type: :request do
-  describe "GET /index" do
-    it "succeeds" do
+RSpec.describe 'Homes', type: :request do
+  describe 'GET /index' do
+    it 'succeeds' do
       get root_path
       expect(response).to be_successful
     end
